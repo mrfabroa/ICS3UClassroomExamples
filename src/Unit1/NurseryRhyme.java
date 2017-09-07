ddc304740cdd55fe5651cb21099c2fd84c944110
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-09-07 11:17:52 -0400

    new NurseryRhyme example

---
+++ src/Unit1/NurseryRhyme.java
@@ -0,0 +1,28 @@
+package Unit1;
+
+public class NurseryRhyme {
+
+	public static void main(String[] args) {
+		/*
+		Commented area that is used to explain segments of code.
+        Nursery Rhyme
+        The name of the nursery rhyme is called...
+        "HICKORY, DICKORY, DOCK"
+		 */
+		
+		System.out.println("");
+		System.out.println("******************************");
+		System.out.println(" ");
+		System.out.println("Nursery Rhyme");
+		System.out.println(" ");
+		System.out.println("Hickory, Dickory Dock");
+		System.out.println("The Mouse Ran up the Clock");
+		System.out.println("The clock struck one,");
+		System.out.println("The mouse ran down,");
+		System.out.println("Hickory, Dickory, Dock");
+		System.out.println(" ");
+		System.out.println("******************************");
+
+	}
+
+}
