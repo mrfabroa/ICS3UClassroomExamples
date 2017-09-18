34154ac3dbe7152f19984646ad70bf0504c50461
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-09-18 01:06:04 -0400

    Update from IntelliJ

--- src/unit1/HelloWorld.java
+++ src/unit1/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Say hello
 
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 		
 		int input = stdlib.StdIn.readInt();
 		System.out.println(input);
